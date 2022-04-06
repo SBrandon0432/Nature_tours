26d5841f175248b9ebe6ec774f0c81ffa6b4cb78
sbrandon0432 <sbrandon0432@yahoo.com>
2022-04-06 13:44:42 -0700

    css added for header

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,53 +1,14 @@
 import React from "react";
 import '../../sass/main.scss';
   // import img from '../../../'
+  import Header from '../components/Header/Header'
 const App = () => {
 
   return (
     <div>
-        <header className="header">
-            <div className="header__logo-box">
-              <img
-                src='http://127.0.0.1:55654/client/img/logo-white.png'
-                alt="logo image"
-                className="header__logo"
-                />
-            </div>
 
-                <div className="header__text-box">
-                    <h1 className="heading-primary">
-                        <span className="heading-primary--main"> Outdoors </span>
-                        <span className="heading-primary--sub"> is where life happens </span>
-                    </h1>
+        <Header />
 
-                    <a
-                    href="#"
-                    className="btn btn-white btn-animation">
-                        Discover out Tours
-                    </a>
-                </div>
-
-        </header>
-
-        <main >
-            <section className="section-about">
-                <div className="u-center-text u-margin-bottom-8">
-                    <h2 className="heading-secondary">
-                        Exciting tours for adventurous people
-                    </h2>
-                </div>
-
-                <div className="row">
-                    <div className="col-1-of-2">
-                        text content
-                    </div>
-                    <div className="col-1-of-2">
-                        imgs
-                    </div>
-                </div>
-            </section>
-
-        </main>
     </div>
   )
 }
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header className="header">
+    <div className="header__logo-box">
+        <img
+        src='http://127.0.0.1:58913/client/img/logo-white.png'
+        alt="logo image"
+        className="header__logo"
+        />
+    </div>
+
+        <div className="header__text-box">
+            <h1 className="heading-primary">
+                <span className="heading-primary--main"> Outdoors </span>
+                <span className="heading-primary--sub"> is where life happens </span>
+            </h1>
+
+            <a
+            href="#"
+            className="btn btn-white btn-animation">
+                Discover out Tours
+            </a>
+        </div>
+
+    </header>
+  );
+}
+
+export default Header
