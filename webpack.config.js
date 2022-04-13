9d099aa912e1fdfc4b7bbee8e02caed1780688ad
sbrandon0432 <sbrandon0432@yahoo.com>
2022-04-13 15:55:54 -0700

    webpack update for mp3 content type

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpg|jpeg|gif|mp4|webm)$/i,
+        test: /\.(png|jpg|jpeg|gif|mp4|webm|mp3)$/i,
         type: "asset/resource",
       },
     ],
