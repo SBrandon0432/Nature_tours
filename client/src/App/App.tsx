a147d55756dbf82615e95aa21c24026e968da19e
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-03 16:44:20 -0700

    get version walk back for node modules

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import '../../sass/main.scss';
 import Header from '../components/Header/Header';
 import About from '../components/About/About';
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM  from "react-dom";
-import App from './App/App'
+import App from './App/App';
 import '../sass/main';
 
 ReactDOM.render(
