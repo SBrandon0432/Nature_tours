3056fd5703c51ec8d591e4aaf76c114f7b6eae56
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-08 19:07:28 -0700

    cards files and tours added for building

---
+++ src/components/Cards/Cards.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Cards: React.FC = () => {
+
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+
+export default Cards
---
+++ src/components/Tours/Tours.tsx
@@ -0,0 +1,17 @@
+import React,{useState} from "react";
+import Cards from "../Cards/Cards";
+
+const Tours: React.FC = (props: any) => {
+
+
+
+  return (
+    <section className="section-tours">
+      <div className="row">
+          <Cards />
+      </div>
+    </section>
+  )
+}
+
+export default Tours
