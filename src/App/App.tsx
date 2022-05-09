b77de301c3907416b227fb05620c25f51d6d94e4
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-08 19:10:21 -0700

    section tours padding/margin and background color

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import About from '../components/About/About';
 import Features from '../components/Features/Features';
+import Tours from '../components/Tours/Tours';
 const App = () => {
 
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <About />
         <Features />
+        <Tours />
     </div>
   )
 }
--- src/components/Tours/Tours.tsx
+++ src/components/Tours/Tours.tsx
@@ -7,6 +7,11 @@ const Tours: React.FC = (props: any) => {
 
   return (
     <section className="section-tours">
+      <div className="u-center-text u-margin-bottom-big">
+          <h2 className="heading-secondary">
+              Most popular tours
+          </h2>
+      </div>
       <div className="row">
           <Cards />
       </div>
