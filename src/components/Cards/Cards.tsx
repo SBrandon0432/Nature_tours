02bdd426af120b66bad614c8020eb5c2e4fc2872
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-09 16:40:12 -0700

    map of cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
-const Cards: React.FC = () => {
 
+
+
+  interface IProps {
+    title: string;
+    days: string;
+    people: string;
+    guides: string;
+    sleeping: string;
+    difficulty: string;
+    price: string;
+  }
+
+
+const Cards: React.FC< Array<IProps> | any> = (cards) => {
 
   return (
-    <div>
-      hello
+    <div className="card">
+      {
+        cards.array.map((card: IProps, index:number)=>{
+          return (
+            <div key={index}>
+              <h1>
+                hello
+              </h1>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
 
 
 export default Cards
+
+
--- src/components/Tours/Tours.tsx
+++ src/components/Tours/Tours.tsx
@@ -1,10 +1,39 @@
 import React,{useState} from "react";
 import Cards from "../Cards/Cards";
 
+
 const Tours: React.FC = (props: any) => {
 
-
-
+  const [cards, setCards] = useState([
+    {
+      title: 'The Sea Explorer',
+      days: '3 Day Tours',
+      people: 'Up to 30 people',
+      guides: '2 tour guilds',
+      sleeping: 'Sleep in cozy hotels',
+      difficulty: 'Difficulty: Easy',
+      price: '$275'
+    },
+    {
+      title: 'The Sea Explorer',
+      days: '3 Day Tours',
+      people: 'Up to 30 people',
+      guides: '2 tour guilds',
+      sleeping: 'Sleep in cozy hotels',
+      difficulty: 'Difficulty: Easy',
+      price: '$275'
+    },
+    {
+      title: 'The Sea Explorer',
+      days: '3 Day Tours',
+      people: 'Up to 30 people',
+      guides: '2 tour guilds',
+      sleeping: 'Sleep in cozy hotels',
+      difficulty: 'Difficulty: Easy',
+      price: '$275'
+    }
+  ]) as any
+  console.log(cards);
   return (
     <section className="section-tours">
       <div className="u-center-text u-margin-bottom-big">
@@ -14,7 +43,11 @@ const Tours: React.FC = (props: any) => {
       </div>
       <div className="row">
         <div className="col-1-of-3">
-          <Cards />
+          {
+            cards.length > 0 && (
+              <Cards array={cards}/>
+            )
+          }
         </div>
       </div>
     </section>
