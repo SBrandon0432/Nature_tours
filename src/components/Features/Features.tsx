81b4122b8ce22563932518b3041500e3bfe2a1c5
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-08 17:26:51 -0700

    icons added to map

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -7,19 +7,23 @@ const Features: React.FC = (props: Array< Object> | any) => {
   const [cards, setCards] = useState([
     {
       title:'Explore the world',
-      text: 'Ex veniam consectetur sunt nisi reprehenderit labore nisi dolore cillum sit deserunt commodo proident eu.'
+      text: 'Ex veniam consectetur sunt nisi reprehenderit labore nisi dolore cillum sit deserunt commodo proident eu.',
+      icon: 'icon-basic-world'
    },
    {
      title:'Meet Nature',
-     text: 'Adipisicing ut cillum reprehenderit incididunt labore dolore consectetur Lorem consequat veniam.'
+     text: 'Adipisicing ut cillum reprehenderit incididunt labore dolore consectetur Lorem consequat veniam.',
+     icon: 'icon-basic-compass'
    },
    {
       title:'Find your way',
-      text: 'Sint et voluptate veniam ut pariatur ut sint eu anim ad non culpa ut pariatur ut sint consequat mollit.'
+      text: 'Sint et voluptate veniam ut pariatur ut sint eu anim ad non culpa ut pariatur ut sint consequat mollit.',
+      icon: 'icon-basic-map'
     },
     {
       title:'Live a healthier life',
-      text: 'Irure et consequat mollit incididunt culpa commodo ad aliquip mollit fugiat ut pariatur ut sint.'
+      text: 'Irure et consequat mollit incididunt culpa commodo ad aliquip mollit fugiat ut pariatur ut sint.',
+      icon: 'icon-basic-heart'
    },
   ])
 
@@ -32,7 +36,7 @@ const Features: React.FC = (props: Array< Object> | any) => {
               return (
                 <div className="col-1-of-4" key={index}>
                     <div className="feature-box">
-                        <i className="feature-box__icon icon-basic-world"></i>
+                        <i className={`feature-box__icon ${card.icon}`}></i>
                         <h3 className="heading-tertuary u-margin-bottom-small">{card.title} </h3>
                         <p className="feature-box__text">
                            {card.text}
