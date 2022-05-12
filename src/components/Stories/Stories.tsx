15131a03be05249c5cb73a2d908dcc7a4572a818
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-12 16:21:29 -0700

    added stories title

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -6,6 +6,13 @@ const Stories: React.FC = () => {
 
   return (
     <section className="section-stories">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">
+          We make people genuinely happy
+        </h2>
+      </div>
+
+
 
     </section>
   )
