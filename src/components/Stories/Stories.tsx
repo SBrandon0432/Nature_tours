5aa8be7b8718b2fae9baef1f22943cd45f53e59f
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-13 15:53:11 -0700

    story css for base shape

--- src/components/Stories/Stories.tsx
+++ src/components/Stories/Stories.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import videoMp4 from '../../Assets/img/video.mp4'
 import videoWebm from '../../Assets/img/video.webm'
 import StoryReviews from "../StoryReviews/StoryReviews";
+import imageOne from '../../Assets/img/nat-8.jpg'
+import imageTwo from '../../Assets/img/nat-9.jpg'
 
 
 const Stories: React.FC = () => {
-
+  const [stories, setStories] = useState([
+    {
+      name:'Mary Smith',
+      img: imageOne,
+      heading: 'I had the best week ever with my family',
+      story: 'Do aute mollit ipsum ex veniam ut pariatur culpa id minim cupidatat ad do. Anim sunt sint occaecat incididunt laboris aliqua officia Lorem. Voluptate eiusmod ut non incididunt enim cupidatat in officia magna qui minim. Veniam ea laboris aute id enim reprehenderit culpa.'
+    },
+    {
+      name:'Jack Wilson',
+      img: imageTwo,
+      heading: 'WOW! My life is completly diffrent now!',
+      story: 'Do aute mollit ipsum ex veniam ut pariatur culpa id minim cupidatat ad do. Anim sunt sint occaecat incididunt laboris aliqua officia Lorem. Voluptate eiusmod ut non incididunt enim cupidatat in officia magna qui minim. Veniam ea laboris aute id enim reprehenderit culpa.'
+    }
+  ]);
 
   return (
     <section className="section-stories">
@@ -25,8 +40,13 @@ const Stories: React.FC = () => {
         </h2>
       </div>
 
+        <div className="StoryReviews">
+          <StoryReviews stories={stories}/>
+        </div>
 
-
+      <div className="u-margin-top-huge u-center-text">
+          <a href="/#" className="btn-text">Read All stories &rarr;</a>
+      </div>
     </section>
   )
 
--- src/components/StoryReviews/StoryReviews.tsx
+++ src/components/StoryReviews/StoryReviews.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 
+interface Iprops {
+  name: string;
+  img: string;
+  heading: string;
+  story: string;
+}
 
-const StoryReviews = () => {
-
+const StoryReviews: React.FC<Array<Iprops> | any > = ({stories}) => {
 
   return (
-    <div>
-      hello
-    </div>
+    <>
+      {
+        stories.map((story: Iprops , index: number)=> {
+          return (
+            <div className="row" key={index}>
+              <div className="story">
+                <figure className="story__shape">
+                  <img src={story.img} className='story__image' alt='Person on tour'/>
+                  <figcaption className="story__caption"> {story.name} </figcaption>
+                </figure>
+                <div className="story__text">
+                    <h3 className="heading-tertiary u-margin-bottom-small">{story.heading} </h3>
+                    <p>
+                      {story.story}
+                    </p>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </>
   )
 }
 
