a6a54826d5983c70f2279afb321d9444482dafd6
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-17 16:35:12 -0700

    spelling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = ( ) => {
                 <ul className="footer__list">
                     <li className="footer__item"><a href="/#" className="footer__link"> Company </a></li>
                     <li className="footer__item"><a href="/#" className="footer__link"> Contact us </a></li>
-                    <li className="footer__item"><a href="/#" className="footer__link"> Carrers </a></li>
+                    <li className="footer__item"><a href="/#" className="footer__link"> Carriers </a></li>
                     <li className="footer__item"><a href="/#" className="footer__link"> Privacy policy </a></li>
                     <li className="footer__item"><a href="/#" className="footer__link"> Terms </a></li>
                 </ul>
