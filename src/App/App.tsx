7c2b1c4f145281a785f0de37f61f3c4f4ebc3e55
sbrandon0432 <sbrandon0432@yahoo.com>
2022-05-23 21:14:07 -0700

    code clean up

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-// import '../../sass/main.scss';
 import Header from '../components/Header/Header';
 import About from '../components/About/About';
 import Features from '../components/Features/Features';
@@ -7,6 +5,7 @@ import Tours from '../components/Tours/Tours';
 import Stories from '../components/Stories/Stories';
 import ContactForm from '../components/ContactForm/ContactForm';
 import Footer from '../components/Footer/Footer';
+
 const App = () => {
 
   return (
